eef7e2b6b4ee627b630437fa6a934fbc0ce63ce0
sandhyachan <sandhyachan6@gmail.com>
2024-12-30 14:39:45 +0530

    Add JSDoc comments for function understanding

--- utils/JwtUtil.js
+++ utils/JwtUtil.js
@@ -1,9 +1,17 @@
 const jwt = require('jsonwebtoken')
-require('dotenv').config();
+require('dotenv').config()
 
-function generateJwtToken(data = {}, SECRET_KEY = process.env.JWT_SECRET) {
-    const token = jwt.sign(data, SECRET_KEY)
+/**
+ * Function to generate a JWT token
+ * @param {Object} data - The payload for the token
+ * @param {String} SECRET_KEY - The secret key for signing the JWT
+ * @param {String} expiresIn - Token expiration time (default: 5 hours)
+ * @returns {String} - The generated JWT token
+ */
+
+function generateJwtToken(data = {}, SECRET_KEY = process.env.JWT_SECRET, expiresIn = '5h') {
+    const token = jwt.sign(data, SECRET_KEY, { expiresIn })
     return token
 }
 
-console.log(generateJwtToken())
+module.exports = { generateJwtToken }
